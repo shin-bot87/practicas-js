bc04b391d5804e08d857e7b0bd8f0e004ebd7b61
shin-bot87 <dapenamorilloivan@gmail.com>
2021-12-17 14:46:05 +0100

    SEGUNDO CAMBIO Conversion a Funciones

--- figuras.js
+++ figuras.js
@@ -1,32 +1,22 @@
 //Codigo del Cuadrado
-console.group("Cuadrados");
-const ladoCuadrado = 5;
-console.log("Los lados del cuadrado miden: " + ladoCuadrado +"cm");
 
-const perimetroCuadrado = ladoCuadrado * 4;
-console.log("El perimetro del cuadrado es: " + perimetroCuadrado + "cm");
+function perimetroCuadrado(lado){
+    return lado * 4;
+} 
 
-const areaCuadrado = ladoCuadrado * ladoCuadrado;
-console.log("El area del cuadrado es: " + areaCuadrado + "cm2");
-console.groupEnd();
+function areaCuadrado(lado){
+    return lado * lado;
+}
 
 //Codigo del Triangulo
-console.group("Triangulos");
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
 
-console.log("Los lados del triangulo miden: " + ladoTriangulo1 + "cm, " + ladoTriangulo2 + "cm, " + baseTriangulo + "cm");
+function perimetroTriangulo(lado1, lado2, base){
+    return lado1 + lado2 + base;
+}
 
-const alturaTriangulo = 5.5;
-console.log("La altura del triangulo es: " + alturaTriangulo + "cm");
-
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
-console.log("El perimetro del triangulo es: " + perimetroTriangulo);
-
-const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
-console.log("El area del triangulo: " + areaTriangulo);
-console.groupEnd();
+function areaTriangulo(base, altura){
+    return (base * altura) / 2;
+}
 
 //Codigo del Circulo
 console.group("Circulo");
