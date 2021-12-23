dac8e18f5753898e8725441334b351718435a22d
shin-bot87 <dapenamorilloivan@gmail.com>
2021-12-23 11:38:55 +0100

    OCTAVO CAMBIO Se añaden Moda y Salarios

---
+++ 5 - Salarios/salarios.js
@@ -0,0 +1,31 @@
+const colombia = [];
+
+colombia.push({
+    name: "Juanita",
+    salary: 1000,
+});
+
+colombia.push({
+    name: "Natalia",
+    salary: 500,
+});
+
+colombia.push({
+    name: "Luisa",
+    salary: 850,
+});
+
+colombia.push({
+    name: "Angie",
+    salary: 700,
+});
+
+colombia.push({
+    name: "Alex",
+    salary: 1500,
+});
+
+colombia.push({
+    name: "Maribel",
+    salary: 300,
+});
